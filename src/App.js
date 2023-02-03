5e335f9535da45d1fb74ccab4744b0701abdd30b
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-03 03:31:33 -0800

    ساخت فانکشنال کامپوننت

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { Component } from "react";
 
 // نکته در مورد رندر: فقط یک تگ را برمیگرداند دوتا باشد خطا می دهد
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>In The Name Of God</h1>
-        <h1>In The Name Of God</h1>
-      </>
-    );
-  }
-}
+
+//shortcut class component: cc
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <h1>In The Name Of God</h1>
+//         <h1>In The Name Of God</h1>
+//       </>
+//     );
+//   }
+// }
+
+//export default App;
+
+
+//shortcut functional component: sfc
+const App = () => {
+  return (
+    <>
+      <h1>In The Name Of God</h1>
+      <h1>In The Name Of God</h1>
+    </>
+  );
+};
 
 export default App;
