63bf0b71d8ff75065e30062f02d3c502bca3f251
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-03 03:22:45 -0800

    استفاده از ری اکت فرگمنت برای برگرداندن چند تگ بدون استفاده از تگ دیگری

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Component } from "react";
 
 // نکته در مورد رندر: فقط یک تگ را برمیگرداند دوتا باشد خطا می دهد
-class App extends Component{
-    render(){
-        return <h1>In The Name Of God</h1>
-    };
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>In The Name Of God</h1>
+        <h1>In The Name Of God</h1>
+      </>
+    );
+  }
 }
 
 export default App;
