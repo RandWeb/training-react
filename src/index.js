38b5728129e53def5d4c4474b6848fb82f4ac364
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-03 04:24:26 -0800

    اضافه کردن بوت استرپ

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-// const element = <h1>Hello world</h1>;
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Product from "./components/product/Product";
 
-// //const element = createElement("h1",{},"hello world");
+const container = document.getElementById("root");
 
-// const container = document.getElementById("root");
+const root = createRoot(container);
 
-// const root = createRoot(container);
-
-// root.render(<App />);
-
-import Talk,{ Walk } from "./Person";
-Talk();
-Walk();
+root.render(<Product />);
