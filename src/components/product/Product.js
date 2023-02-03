8a5a05609a012d07e92cf670848bb029f0583cee
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-03 06:24:57 -0800

    آموزش استیت هر کامپوننت و ایونت ها

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,34 +1,57 @@
 import { Component } from "react";
-
+import "./Product.css";
 class Product extends Component {
-  state = {};
+  state = {
+    id: 1,
+    title: "name product",
+    count: 0,
+    imageUrl: "https://loremflickr.com/320/240",
+  };
   render() {
-    const products = [
-      { id: 1, title: "product 1" },
-      { id: 2, title: "product 2" },
-      { id: 3, title: "product 3" },
-    ];
-
-    const format = (count) => {
-      if (count === 0) return "zero";
-      return count;
-    };
-
     return (
       <>
-        <span className="m-2 text-info">name product</span>
-        <span className="m-2 badge bg-primary">{format(0)}</span>
-        <button className="m-2 btn btn-sm btn-success">+</button>
-        <button className="m-2 btn btn-sm btn-warning">-</button>
-        <button className="m-2 btn vtn-sm btn-danger">delete</button>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>{product.title}</li>
-          ))}
-        </ul>
+        <span className="m-2 text-info">{this.state.title}</span>
+        <span className="m-2 badge bg-primary">
+          {this.state.count !== 0 ? this.state.count : "zero"}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="m-2 btn btn-sm btn-success"
+        >
+          +
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="m-2 btn btn-sm btn-warning"
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            this.handleDelete(this.state.id);
+          }}
+          className="m-2 btn vtn-sm btn-danger"
+        >
+          delete
+        </button>
+        <img src={this.state.imageUrl} />
       </>
     );
   }
+
+  handleIncrement = () => {
+    console.log("increment");
+    this.setState({ count: ++this.state.count });
+  };
+  handleDecrement = () => {
+    console.log("decrement");
+    const { count } = this.state;
+    if (count === 0) return alert("count product is zero");
+    this.setState({ count: --count });
+  };
+  handleDelete = (id) => {
+    console.log(id);
+  };
 }
 
 export default Product;
