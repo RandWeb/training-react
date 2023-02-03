235934e06b84bb4f0805646263aec0281b291433
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-03 03:53:41 -0800

    استفاده از Export default

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-const element = <h1>Hello world</h1>;
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// const element = <h1>Hello world</h1>;
 
-//const element = createElement("h1",{},"hello world");
+// //const element = createElement("h1",{},"hello world");
 
-const container = document.getElementById("root");
+// const container = document.getElementById("root");
 
-const root = createRoot(container);
+// const root = createRoot(container);
 
-root.render(<App />);
+// root.render(<App />);
+
+import Talk,{ Walk } from "./Person";
+Talk();
+Walk();
