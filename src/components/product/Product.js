34f6bd683d2a7ab4166568976586bc0bb5f0b2dd
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-03 04:54:42 -0800

    استفاده از مپ برای تغییر شکل ابجکت ها

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -3,13 +3,29 @@ import { Component } from "react";
 class Product extends Component {
   state = {};
   render() {
+    const products = [
+      { id: 1, title: "product 1" },
+      { id: 2, title: "product 2" },
+      { id: 3, title: "product 3" },
+    ];
+
+    const format = (count) => {
+      if (count === 0) return "zero";
+      return count;
+    };
+
     return (
       <>
         <span className="m-2 text-info">name product</span>
-        <span className="m-2 badge bg-primary">2</span>
+        <span className="m-2 badge bg-primary">{format(0)}</span>
         <button className="m-2 btn btn-sm btn-success">+</button>
         <button className="m-2 btn btn-sm btn-warning">-</button>
         <button className="m-2 btn vtn-sm btn-danger">delete</button>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
       </>
     );
   }
